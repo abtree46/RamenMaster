4adc2fcc0514d3ace993f55bb68b973f913e1f92
Nakwon Yoon <yoonnw1000@gmail.com>
2020-07-15 13:30:44 -0500

    adding skel

--- server.js
+++ server.js
@@ -1,13 +1,45 @@
-// Dependencies
-// =============================================================
 var express = require("express");
 var path = require("path");
 
-// Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = 3000;
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+var reservation = [
+
+]
+
+var waitList = [
+
+]
+
+
+app.get("/", function(req , res){
+    res.sendFile(path.join(__dirname, "index.html"))
+});
+
+app.get("/table", function(req, res){
+    res.sendFile(path.join(__dirname, "table.html"))
+});
+
+app.get("/reservation", function(req, res){
+    res.sendFile(path.join(__dirname, "reservation.html"))
+})
+
+app.get("/api/watilist", function (req, res){
+    return res.json(waitList)
+});
+
+app.get("/api/reservation", function (req,res){
+    return res.json(reservation);
+})
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT" + PORT);
+})
