6598eaef390d04733266b4d0af288abb5805ebbc
Janella Senica <janella.senica@gmail.com>
2020-07-16 10:09:50 -0500

    rev html and js

--- logic.js
+++ logic.js
@@ -1,4 +1,13 @@
 var tableBtn = $("tablesBtn");
 var resBtn = $("resBtn");
 
+$(document).ready(function () {
+    $("#tablesBtn").click(function () {
+    });
+
+    $("#resBtn").click(function () {
+    });
+
+});
+
 
