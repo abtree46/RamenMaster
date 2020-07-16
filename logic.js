9cb861f292d3789961bfa8544b8ea390ed9d3c3e
Ashley Bleibtrey <ashleybleibtrey1@gmail.com>
2020-07-16 10:59:35 -0500

    added logic to get form info

--- logic.js
+++ logic.js
@@ -9,5 +9,35 @@ $(document).ready(function () {
     });
 
 });
+$(".submit").on("click", function(){
 
+    var reservation = {
+        customerName: $('#reserve_name').val().trim(),
+        phoneNumber: $('#reserve_phone').val().trim(),
+        customerEmail: $('#reserve_email').val().trim(),
+        customerID: $('#reserve_uniqueID').val().trim()
+    };
 
+    console.log(reservation);
+
+    var currentURL = window.location.origin;
+
+    $.post(currentURL + "/api/table", reservation,
+    function(data){
+        if(data == true){
+            alert("You got it!")
+        }
+        if(data == false){
+            alert("You are on the waiting list!")
+        }
+
+        $('#reserve_name').val("");
+        $('#reserve_phone').val("");
+        $('#reserve_email').val("");
+        $('#reserve_uniqueID').val("");
+
+    });
+
+return false;
+
+});
